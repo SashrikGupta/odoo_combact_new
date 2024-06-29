8fa3a8773c5617874f47d9f75f95fbb734deb82a
SashrikGupta <sashrikgupta@gmail.com>
2024-06-29 12:02:04 +0530

    intiall commit

---
+++ backend server/models/teachers.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const teacherSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  students: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+    },
+  ],
+  events: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+    },
+  ],
+  urls: [
+    {
+      type: String,
+    },
+  ],
+});
+
+const Teacher = mongoose.model("Teacher", teacherSchema);
+
+module.exports = Teacher;
